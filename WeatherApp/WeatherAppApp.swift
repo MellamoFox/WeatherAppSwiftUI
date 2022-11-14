48f29b2c7b01a32119824c07d0afebfbe4a6d375
MellamoFox <104905904+MellamoFox@users.noreply.github.com>
2022-11-14 15:22:11 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by VT on 14.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
