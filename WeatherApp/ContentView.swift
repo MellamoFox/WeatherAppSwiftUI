612031aedcbb6c9b8329df1168676e3b40fe3fe5
MellamoFox <104905904+MellamoFox@users.noreply.github.com>
2022-11-16 14:50:07 +0200

    updated

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -9,18 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .yellow]),
+                        startPoint: .topLeading,
+                        endPoint: .bottomLeading )
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Kiev, UA")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                VStack(spacing: 10) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                        Text("76 °")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                HStack {
+                    WeatherDayView(dayofWeek: "TUE",
+                                imageName: "cloud.sun.fill",
+                                temperature: 75)
+                }
+
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    
+    var dayofWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        HStack(spacing: 30) {
+            VStack(alignment: .trailing) {
+                Text(dayofWeek)
+                    .foregroundColor(.white)
+                Image(systemName: imageName)
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 40, height: 40)
+                
+                Text("\(temperature)°")
+                    .foregroundColor(.white)
+            }
+            
+        }
     }
 }
